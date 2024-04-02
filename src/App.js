db9807929258553dde57639edd5e204ab0c9a0c3
Amanda Rahmat Hidayat <amanda.rahmat@ai.astra.co.id>
2024-04-02 19:21:56 +0700

    create portfolio part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/section/Navbar";
+import Footer from "./components/section/Footer";
+import "./App.css";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MySkills from "./components/pages/MySkills";
+import MyProjects from "./components/pages/MyProjects";
+import Achievement from "./components/pages/Achievement";
+import Certification from "./components/pages/Certification";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Testing amanda
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/achievement" component={Achievement} />
+          <Route path="/myskills" component={MySkills} />
+          <Route path="/myprojects" component={MyProjects} />
+          <Route path="/certification" component={Certification} />
+        </Switch>
+        <Footer />
+        <div className="social">
+          <a
+            href="https://www.instagram.com/amndrahmat/"
+            target="_blank"
+            aria-label="Instagram"
+            rel="noreferrer"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/amandarahmath/"
+            target="_blank"
+            aria-label="LinkedIn"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a
+            href="https://github.com/amndrahmat"
+            target="_blank"
+            aria-label="Github"
+            rel="noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+      </Router>
+    </>
   );
 }
 
